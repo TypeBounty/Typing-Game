9062bee1f6c0331a4dd2873f9e0cba62b85b7824
AnZamora <anzamorawhs@gmail.com>
2020-04-27 14:06:47 -0700

    Bot

---
+++ Type Bounty/Assets/BottomWM/BotWordSpawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BotWordSpawner : MonoBehaviour
+{
+    public GameObject wordPrefab;
+    public Transform wordCanvas;
+
+    public BotWordDisplay SpawnWord()
+    {
+        Vector3 randomPosition = new Vector3(Random.Range(-.5f, 2.5f), Random.Range(-1f, 5f));
+
+        GameObject wordObj = Instantiate(wordPrefab, randomPosition, Quaternion.identity, wordCanvas);
+        BotWordDisplay wordDisplay = wordObj.GetComponent<BotWordDisplay>();
+
+        return wordDisplay;
+    }
+}
