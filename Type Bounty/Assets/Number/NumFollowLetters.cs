1bcf2bdc8458fbdc299c61dbfe166f8e8ca9efd1
TypeBounty <46985768+jchavez58@users.noreply.github.com>
2020-04-27 14:04:38 -0700

    Num game

---
+++ Type Bounty/Assets/Number/NumFollowLetters.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumFollowLetters : MonoBehaviour
+{
+    public NumWordDisplay removeWord;
+    public NumWordDisplay wordDisplay;
+
+    Vector3 tempPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (NumWordDisplay.moveShip == true)
+        {
+            tempPos = transform.position;
+            tempPos.x = NumWordDisplay.wordPosx;
+            tempPos.y = NumWordDisplay.wordPosy;// - 800;
+            transform.position = tempPos;
+        }
+        /* do
+         {
+             tempPos = transform.position;
+
+             tempPos.x += 2.0f;
+             tempPos.y -= 2.0f;
+
+             transform.position = tempPos;
+
+         } while (false);
+         */
+    }
+
+    }
