4f681c82eb603c56103a576534a58a83f6ae7ab4
dfelix4csub <dfelix.csub@gmail.com>
2020-04-27 13:07:01 -0700

    Sym

---
+++ Type Bounty/Assets/Symbols/SymFollowLetters.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SymFollowLetters : MonoBehaviour
+{
+    public SymWordDisplay removeWord;
+    public SymWordDisplay wordDisplay;
+
+    Vector3 tempPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SymWordDisplay.moveShip == true)
+        {
+            tempPos = transform.position;
+            tempPos.x = SymWordDisplay.wordPosx;
+            tempPos.y = SymWordDisplay.wordPosy;// - 800;
+            transform.position = tempPos;
+        }
+        /* do
+         {
+             tempPos = transform.position;
+
+             tempPos.x += 2.0f;
+             tempPos.y -= 2.0f;
+
+             transform.position = tempPos;
+
+         } while (false);
+         */
+    }
+
+    }
---
+++ Type Bounty/Assets/Symbols/SymWordManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SymWordManager : MonoBehaviour
+{
+    public List<SymWord> words;
+
+    public SymWordSpawner wordSpawner;
+
+    private bool hasActiveWord;
+    private SymWord activeWord;
+
+    private void Start()
+    {
+        AddWord();
+    }
+
+    public void AddWord()
+    {
+        SymWord word = new SymWord(SymWordGenerator.GetRandomWord(), wordSpawner.SpawnWord());
+        Debug.Log(word.word);
+
+        words.Add(word);
+    }
+
+    public void TypeLetter(char letter)
+    {
+        if (hasActiveWord)
+        {
+            //Check if letter was next
+            //Remove it from the word
+            if (activeWord.GetNextLetter() == letter)
+            {
+                activeWord.TypeLetter();
+            }
+
+        }
+        else
+        {
+            foreach (SymWord word in words)
+            {
+                if (word.GetNextLetter() == letter)
+                {
+                    activeWord = word;
+                    hasActiveWord = true;
+                    word.TypeLetter();
+                    break;
+                }
+            }
+        }
+
+        if (hasActiveWord && activeWord.WordTyped())
+        {
+            AddWord();
+            hasActiveWord = false;
+            words.Remove(activeWord);
+        }
+    }
+
+    /*public static void getOut()
+    {
+        words.Remove(activeWord);
+    }*/
+
+}
