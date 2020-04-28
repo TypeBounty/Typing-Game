44ec4eff2b9d9d60691e351b752c12231de6500d
dfelix4csub <dfelix.csub@gmail.com>
2020-04-28 01:05:35 -0700

    Mid & Back

---
+++ Type Bounty/Assets/Mid/MidWordGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MidWordGenerator : MonoBehaviour
+{
+    private static string[] wordList = { "a", "s", "d", "f", "g", "h", "j", "k", "l", "as",
+                                         "sad", "dash", "fall", "gas", "had", "has", "lads"};
+
+    public static string GetRandomWord()
+    {
+        int randomIndex = Random.Range(0, wordList.Length);
+        string randomWord = wordList[randomIndex];
+
+        return randomWord;
+    } 
+}
