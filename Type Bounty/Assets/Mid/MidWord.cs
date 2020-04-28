defc0454e4c9d3301859f9ca4d6b1f46a8db9162
AnZamora <anzamorawhs@gmail.com>
2020-04-28 01:29:56 -0700

    additions

---
+++ Type Bounty/Assets/Mid/MidWord.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class MidWord
+{
+    public string word;
+    private int typeIndex;
+
+    private MidWordDisplay display;
+    public MidWordManager wordManager;
+
+    public MidWord(string _word, MidWordDisplay _display)
+    {
+        word = _word;
+        typeIndex = 0;
+
+        display = _display;
+        display.SetWord(word);
+    }
+
+    public char GetNextLetter()
+    {
+        return word[typeIndex];
+    }
+
+
+    public void TypeLetter()
+    {
+        typeIndex++;
+        //Remove letter on screen
+        display.RemoveLetter();
+    }
+
+    public bool WordTyped()
+    {
+        bool wordTyped = (typeIndex >= word.Length);
+        if (wordTyped)
+        {
+            //Update Score
+            MidScore.scoreVal++;
+
+            //Update Position of Ship
+
+            //Remove the word from the screen
+            display.removeWord();
+            
+        }
+        return wordTyped;
+    }
+}
