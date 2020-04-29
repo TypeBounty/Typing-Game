e8d1f04b9396421e238de6dcc6bbe39a46852de4
dfelix4csub <dfelix.csub@gmail.com>
2020-04-28 23:34:50 -0700

    WordManager

--- Type Bounty/Assets/Keyboard Game/FollowLetters.cs
+++ Type Bounty/Assets/Keyboard Game/FollowLetters.cs
@@ -4,28 +4,37 @@ using UnityEngine;
 
 public class FollowLetters : MonoBehaviour
 {
+    public WordDisplay removeWord;
+    public WordDisplay wordDisplay;
+
     Vector3 tempPos;
     // Start is called before the first frame update
     void Start()
     {
-       
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        do
+        if (WordDisplay.moveShip == true)
         {
             tempPos = transform.position;
-
-            tempPos.x += 2.0f;
-            tempPos.y -= 2.0f;
-
+            tempPos.x = WordDisplay.wordPosx;
+            tempPos.y = WordDisplay.wordPosy;// - 800;
             transform.position = tempPos;
+        }
+        /* do
+         {
+             tempPos = transform.position;
 
-        } while (false);
+             tempPos.x += 2.0f;
+             tempPos.y -= 2.0f;
 
-      
+             transform.position = tempPos;
+
+         } while (false);
+         */
     }
 
 }
