27bba9c7ede1501749b13cfe64bbb9562108a0a0
dfelix4csub <dfelix.csub@gmail.com>
2020-04-29 20:55:18 -0700

    Timer and Score

--- Type Bounty/Assets/Top/TopScore.cs
+++ Type Bounty/Assets/Top/TopScore.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 
 public class TopScore : MonoBehaviour
 {
-    public static int scoreVal = 0;
+    /*public static int scoreVal = 0;
     Text score;
 
     // Start is called before the first frame update
@@ -24,5 +24,34 @@ public class TopScore : MonoBehaviour
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 4);
         }
+    }*/
+
+    public static int scoreVal;
+    public Text score;
+    public int scores;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scores = scoreVal;
+        scoreVal = 0;
+        score = GetComponent<Text>();
+
+
     }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        score.text = "Score: " + scores;
+        if (scoreVal == 10)
+        {
+            SceneManager.LoadScene("WordMangerScoreScreen");
+        }
+    }
+
 }
